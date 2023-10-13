144a06d8c9ac3810d7e7478253e0957df263a635
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 02:10:25 -0300

    Add files via upload

---
+++ Ex11/Ex11/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int indice = 0;
+            double[,] m = new double[3, 3];
+            double[,] m2 = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    m[i,j] = double.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0;i < 3; i++)
+            {
+                for(int j = 0;j < 3; j++)
+                {
+                    indice = i + j;
+
+                    if (indice != 0)
+                    {
+                        m2[i,j] = m[i,j] / indice;
+                    }
+                    else
+                    { //ele recebe o valor de 0 pois o índice está no valor de 0, então todo número dividido por 0 = 0
+                        m2[i, j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0;i<3; i++)
+            {
+                for( int j = 0;j<3; j++)
+                {
+                    Console.Write(m2[i,j] + "  ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
