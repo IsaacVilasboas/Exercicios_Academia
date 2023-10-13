10535f99be360a30319bb137e4ffb18e6908bf1f
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:11:32 -0300

    Add files via upload

---
+++ Ex09/Ex09/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+
+                Console.WriteLine("digite um número inteiro positivo");
+                int numero = int.Parse(Console.ReadLine());
+
+                while (numero <= 0)
+                {
+                    Console.WriteLine("número invalido");
+                    numero = int.Parse(Console.ReadLine());
+                }
+                if (numero > 0)
+                {
+                    Console.WriteLine("número digitado foi: " + numero);
+                    for (int i = 2; i < numero; i += 2)
+                    {
+                        Console.Write(i + ",");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Deseja informar outro número (s/n)? ");
+            } while (Console.ReadLine().ToLower() == "s");
+        }
+    }
+}  
