5491b9722f8225c811142bb924f17bafe6cea912
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:43:28 -0300

    Add files via upload

---
+++ Ex05/Ex05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[5];
+
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+
+            int menor = v[0];
+            int posição = 0;
+
+            for (int i = 1; i < v.Length; i++) {
+
+                if (v[i] < menor)
+                {
+                    menor = v[i];
+                    posição = i;
+                }
+            }
+            Console.WriteLine("O menor valor é " + menor + " e está na posição " + posição);
+
+        }
+    } 
+}
+
