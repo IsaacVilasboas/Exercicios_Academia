c97725c0ee98d2f6d6f0631da75e17ba11513d5a
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 02:14:05 -0300

    Add files via upload

---
+++ Ex16/Ex16/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ex16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] m = new int[3, 4];
+            int[,] n = new int[4, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    m[i,j] = random.Next(10);
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(m[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int j = 0; j < 4; j++)
+            {
+                for(int i = 0;i < 3; i++)
+                {
+                    n[j,i] = m[i,j];
+                }
+            }
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(n[j,i] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Ex17/Ex17/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Ex17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] m = new int[10,10];
+
+            int valor = 0;
+            int max = 0;
+            int linha = 0;
+            int coluna = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    m[i, j] = random.Next(1,101);
+                }
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 0;j < 10; j++)
+                {
+                    Console.Write(m[i,j] + "   ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0;i < 10; i++)
+            {
+                for(int j = 0;j < 10; j++)
+                {
+                    valor = m[i,j];
+
+                    if (valor > max)
+                    {
+                        max = valor;
+                        linha = i;
+                        coluna = j;
+                    } 
+                }
+            }
+            int menorLinha = m[linha, 0];
+            for ( int j = 0; j < 10; j++)
+            {
+                if (m[linha, j] < menorLinha) { 
+                    {
+                      menorLinha = m[linha, j];
+                    }
+                }
+            }
+            Console.WriteLine("Maior " + max);
+            Console.WriteLine("Menor " + menorLinha);
+            Console.WriteLine("Linha " + linha);
+            Console.WriteLine("Coluna " + coluna);
+        }
+    }
+}
