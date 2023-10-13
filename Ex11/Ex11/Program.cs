302a2575f55b7a3d55684da8024e815f3518384c
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 00:50:56 -0300

    Add files via upload

---
+++ Ex11/Ex11/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, minutos;
+            Console.WriteLine("Que horas é agora ?");
+            horas = int.Parse(Console.ReadLine());
+            minutos = horas * 60;
+
+            if (horas < 23.59 && minutos > 60)
+            {
+                Console.WriteLine("Hora valida");
+            }
+        }
+    }
+}
