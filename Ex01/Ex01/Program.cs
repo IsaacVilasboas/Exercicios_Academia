44f1905031c8fd25526011d75db8dc4b112670ac
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 02:04:59 -0300

    Add files via upload

---
+++ Ex01/Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5, 3];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Informe os valores para a primeira coluna:");
+                matriz[i, 0] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                matriz[i, 1] = matriz[i, 0] + 10;
+               
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                matriz[i, 2] = matriz[i, 0] * 2;
+            }
+            Console.WriteLine("Resultado:");
+            for (int i = 0;i < 5; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write( "[" + matriz[i, j] + "]");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Ex03/Ex03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] m = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("Coluna " + i + " digite o valores da " + (j + 1) + " linha: ");
+                    m[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0;i < 4; i++)
+            {
+
+                for(int j = 0;j < 4; j++)
+                {
+                    Console.Write( "[" + m[i, j] + "]");
+                }
+                Console.WriteLine(i);
+            }
+            Console.Write("A matriz principal é ");
+            Console.WriteLine();
+            for (int i = 0; i < 4; ++i)
+            {
+                Console.WriteLine(m[i, i] + " ");
+            }
+        }
+    }
+}
