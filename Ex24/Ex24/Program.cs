2b084eca822fabb33170856ac5c4db97a0d8ecf0
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:34:20 -0300

    Program.cs

--- Ex24/Ex24/Program.cs
+++ Ex24/Ex24/Program.cs
@@ -7,7 +7,7 @@
             double cont;
             for (int i = 15; i <= 200; i++)
             {
-                //cont = i * i; essa seria uma forma mais facil de fazer.
+                
                 cont = Math.Pow(i, 2);
                 Console.WriteLine(cont);
             }
