acf4a2d193ddbc5e79598092ba0c049a96e1f6b4
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 02:08:41 -0300

    Add files via upload

---
+++ Ex07/Ex07/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Ex07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] m = new int[3, 3];
+            int[,] m2 = new int[3, 3];
+            int[,] m3 = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    m[i, j] = random.Next(10);
+                    Console.Write(m[i,j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    m2[i, j] = random.Next(10);
+                    Console.Write(m2[i,j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    m3[i, j] = m[i, j] - m2[i, j];
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(m3[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Ex09/Ex09/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] m = new int[3, 3];
+            Random random = new Random();
+
+            Console.WriteLine("Digite um número;");
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    m[i, j] = random.Next(0, 6);
+                }
+            }
+            bool numero = false;
+            for (int i = 0;i < 3; i++)
+            {
+                for(int j = 0;j < 3; j++)
+                {
+                    if (m[i, j] == n)
+                    {
+                        numero = true;
+                        break;
+                    }
+                }
+            }
+            if (numero)
+            {
+                Console.WriteLine("O número " + n + " existe na matriz");
+            }
+            else
+            {
+                Console.WriteLine("O número " + n + " não existe na matriz");
+            }
+        }
+    }
+}
