f6c626c64c562b4490af4cb05bfd46541996853f
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:16:50 -0300

    Add files via upload

---
+++ Ex13/Ex13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int idade;
+            idade = 0;
+            while (true)
+            {
+                Console.WriteLine("Digite a sua idade :");
+                idade  = int.Parse(Console.ReadLine());
+                if (idade >= 0) {
+                    Console.WriteLine("Idade valida");
+                    break;
+                }
+                else if ( idade <= 0) {
+                    Console.WriteLine("Idade invalida. Tente novamente ");
+                } 
+            }
+            Console.WriteLine(idade);
+        }
+    }
+}
