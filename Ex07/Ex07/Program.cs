943566f30bee4ba24b50ed37824053363f52e165
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:10:04 -0300

    Add files via upload

---
+++ Ex07/Ex07/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex07;
+
+class Program
+{
+    static void Main()
+    {
+        int n2= 0;
+        int n1 = 0;
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                n2++;
+            }
+            else
+            {
+                n1++;
+            }
+        }
+
+        Console.WriteLine("Quantidade de números pares: " + n2);
+        Console.WriteLine("Quantidade de números ímpares: " + n1);
+    }
+}
