7ca752d7903758ceece41045bd2bb323c79c9de7
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 02:06:51 -0300

    Add files via upload

---
+++ Ex04/Ex04/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] m = new int[5, 5];
+            int pares = 0;
+            int impares = 0;
+            int positivos = 0;
+            int negativos = 0;
+            int zeros = 0;
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    m[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 5; ++i)
+            {
+                for(int j = 0; ++j < 5; ++j)
+                {
+                    if(m[i,j] % 2 == 0)
+                    {  
+                        pares++;
+                    }
+                    if (m[i,j] % 2 != 0)
+                    {
+                        impares++;
+                    }
+                }
+            }
+            for (int i = 0;i < 5; ++i)
+            {
+                for( int j = 0;j < 5; ++j)
+                {
+                    if (m[i,j] > 0)
+                    {
+                        positivos++;
+                    }
+                    if (m[i,j] < 0)
+                    {
+                        negativos++;
+                    }
+                    if (m[i,j] == 0)
+                    {
+                        zeros++;
+                    } 
+                }
+            }
+            Console.WriteLine(pares + " pares");
+            Console.WriteLine(impares + " ímpares");
+            Console.WriteLine(positivos + " positivos");
+            Console.WriteLine(negativos + " negativos");
+            Console.WriteLine(zeros + " zeros");
+        }
+    }
+}
---
+++ Ex05/Ex05/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Ex05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] m = new double[2, 5];
+            double[,] m2 = new double[2, 5];
+            double[,] m3 = new double[2, 5];
+            
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("Digite o valor da linha " + (i+1));
+                    m[i,j] = double.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0;i < 2; i++)
+            {
+                for(int j = 0;j < 5; j++)
+                {
+                    Console.WriteLine("Digite o valor da linha " + (i + 1));
+                    m2[i,j] = double.Parse(Console.ReadLine());
+                }
+            }
+            for(int i = 0;i < 2; i++)
+            {
+                for(int j = 0; j<5; j++)
+                {
+                    m3[i,j] = m2[i,j] + m[i,j];
+                }
+            }
+            Console.WriteLine();
+            for(int i = 0; i < 2; i++)
+            {
+                for( int j = 0; j < 5; j++)
+                {
+                    Console.Write("[ " + m3[i,j]+" ]");
+                }
+              Console.WriteLine(i);    
+            }
+        }
+    }
+}
