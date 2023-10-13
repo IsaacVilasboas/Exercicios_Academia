5cd124c28fd9edae08f5cee6071450c1f1f62706
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:48:16 -0300

    Add files via upload

---
+++ Ex07/Ex07/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[10];
+            int pares = 0;
+            int impares = 0;
+            for (int i = 0; i < 10; i++) {
+
+                v[i] = int.Parse(Console.ReadLine());
+            }
+        
+            for (int i = 0;i < 10; i++)
+            {
+                if (v[i] % 2 == 0) 
+                {
+                    Console.Write(v[i]);
+                }
+            }
+            Console.Write(' ');
+            for (int i = 0;i < 10; i++) {
+                
+                if (v[i] % 2 != 0)
+                {
+                    Console.Write(v[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Ex08/Ex08/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] v = new double[30];
+            int casa = 0;
+
+            for (int i = 0; i < 30; i++)
+            {
+                v[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            for (int i = 0;i < 30; i++)
+            {
+              
+                if (v[i] < 0) {
+                    casa = i;
+                    Console.Write((casa + 1)+" ");
+                }
+            }
+        }
+    }
+}
