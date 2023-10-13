e9426ebe40265a673b147914fb0dc52516182b26
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:40:30 -0300

    Add files via upload

---
+++ Ex01/Ex01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v;
+            int n = int.Parse(Console.ReadLine());
+            v = new int[n];
+            int impares = 0;
+            int pares = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+                v[i] = valor;  
+            }
+            for(int i = 0; i < n; i++)
+            {
+                if (v[i] % 2 == 0)
+                {
+                    pares++;
+                }
+                else
+                {
+                    impares++;
+                }
+            }           
+            Console.WriteLine("Teve " + impares + " números impares");
+            Console.WriteLine("Teve " + pares + " números pares");
+        }
+    }
+}
---
+++ Ex03/Ex03/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ex03
+
+{
+    internal class Program
+    {
+        // Função para verificar se um número é primo
+        static bool EhPrimo(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        static void Main()
+        {
+            int[] v = new int[5];
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("posições dos números primos:");
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (EhPrimo(v[i])) //função EhPrimo verifica para saber se o numero é inteiro
+                {
+                    Console.WriteLine(v[i]);
+                }
+            }
+        }
+    }
+}
