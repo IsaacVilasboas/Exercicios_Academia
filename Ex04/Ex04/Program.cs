4b284c5a51de8ce494d9b4b7a89ef082dcde2143
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:07:54 -0300

    Add files via upload

---
+++ Ex04/Ex04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            int cont;
+            cont = 1;
+            Console.WriteLine("Digite dois valores");
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            while (cont < y)
+            {
+                cont += 1;
+                x = x * x;
+                Console.WriteLine(x);
+            }  
+        }
+    }
+
+}
