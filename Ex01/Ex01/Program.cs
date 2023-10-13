112691493a641c0856d6adf44b380fd02cf681c5
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:05:44 -0300

    Add files via upload

---
+++ Ex01/Ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont, soma, id, media;
+            cont = 0;
+            soma = 0;
+
+            for (int i = cont; i < 4; i++)
+            {
+                cont = cont + 1;
+                Console.WriteLine("Qual a sua idade ?");
+                id = int.Parse(Console.ReadLine());
+                soma = soma + id;
+            }
+            media = soma / cont;
+            Console.WriteLine(media);
+        }
+    }
+}
---
+++ Ex02/Ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior, n;
+            maior = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                
+                if (n >= maior)
+                {
+                    maior = n;
+                   
+                }
+            }
+            Console.WriteLine(maior);
+        }
+    }
+}
