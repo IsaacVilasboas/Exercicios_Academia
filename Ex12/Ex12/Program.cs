03c775dea458376e9fe77bd2bc7230b300919569
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:56:24 -0300

    Add files via upload

---
+++ Ex12/Ex12/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[5];
+            int numero = 0;
+            int poss = 0;
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i < 5; i++)
+            {
+                if (v[i] == n)
+                {
+                    Console.WriteLine("O número informado está na  posição " + i + " e seu valor é: " + v[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Número informado não está dentro do vetor");
+                }
+            }
+        }
+    }
+}
