45d333fa6e2b0abe16465819b3dd5d471f839e0e
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 02:12:26 -0300

    Add files via upload

---
+++ Ex14/Ex14/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] m = new int[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    m[i, j] = random.Next(0,6);
+                }
+            }
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int j = 0; j < 5; ++j)
+                {
+                    Console.Write(m[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0;i < 5; i++)
+            {
+                if (m[i,i] == m[i, 4 - i])
+                {
+                    Console.WriteLine("Os mesmo elementos são :" + m[i,i] + " ");  
+                }
+            }
+        }
+    }
+}
---
+++ Ex15/Ex15/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] m = new int[4, 4];
+            int[,] m2 = new int[4, 4];
+            int[,] r = new int[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    m[i, j] = random.Next(10);
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    m2[i, j] = random.Next(10);
+                }
+            }
+            for(int i = 0;i < 4; i++)
+            {
+                for(int j = 0;j < 4; j++)
+                {
+                    r[i, j] = m[i,j] + m2[i, j];
+                }
+            }
+            for(int i = 0;i < 4 ; i++)
+            {
+                for( int j = 0;j< 4 ; j++)
+                {
+                    Console.Write(r[i,j] + "  " );
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
