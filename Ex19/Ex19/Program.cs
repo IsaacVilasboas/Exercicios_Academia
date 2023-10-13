dd5da3aa3fd3713b24bf37359ecec5310f7414f7
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-10-13 01:23:54 -0300

    Add files via upload

---
+++ Ex19/Ex19/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int cont = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                cont += 1;
+            }
+            Console.WriteLine(cont);
+        }   
+    }
+}
