36a5ee396d983871175bf11707206ee0ece8f47a
IsaacVilasboas <146769876+IsaacVilasboas@users.noreply.github.com>
2023-11-16 20:56:24 -0300

    Add files via upload

---
+++ Atv3/Atv3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Atv3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            p.nome = "isaac";
+            p.idade = 22;
+            p.genero = 'M';
+            Console.WriteLine("Nome: " + p.nome);
+            Console.WriteLine("Idade: " + p.idade);
+            Console.WriteLine("Gênero: " +  p.genero);
+        }
+    }
+}
---
+++ Atv4/Atv4/Jogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv4
+{
+    internal class Jogo
+    {
+        public string titulo;
+        public string genero;
+        public string plataforma;
+        public int lançamento;
+
+    }
+}
+//Título, Gênero, Plataforma (por exemplo, PC, Xbox, PlayStation), Ano de lançamento
+// Na main, instancie um objeto e faça a leitura pelo teclado.
