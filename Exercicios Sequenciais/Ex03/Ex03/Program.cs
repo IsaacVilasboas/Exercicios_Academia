5db946999eb013f64227eae163f4e6d05a6dbc7f
Isaac Vilasboas <isaacvilasboas4@gmail.com>
2023-11-24 19:45:33 -0300

    Organizando Exercicio Sequenciais

---
+++ Exercicios Sequenciais/Ex03/Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
---
+++ Exercicios Sequenciais/Ex05/Ex05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Ex05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double km, l, media;
+
+            Console.WriteLine("quantos Km você PERCORREU: ");
+            km = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos litros gastou: ");
+            l = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            media = km / l;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture) + "l foi a média gasta de combustivel");
+        }
+    }
+}
